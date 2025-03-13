d5b23f8d54ae2eb7c413d50dd7080c48a9271f81
mizza-cz <mizza.cz666@gmail.com>
2025-03-13 12:24:22 +0100

    add fix

--- src/components/evaluationSection/evaluationSection.js
+++ src/components/evaluationSection/evaluationSection.js
@@ -1,40 +1,12 @@
-// document.querySelectorAll(".nav-link").forEach((link) => {
-//   link.addEventListener("click", function () {
-//     const tabTrigger = new bootstrap.Tab(this);
-//     tabTrigger.show();
-
-//     this.addEventListener(
-//       "shown.bs.tab",
-//       () => {
-//         const evaluationSection = document.querySelector("#evaluationSection");
-//         const offset = 20;
-
-//         if (evaluationSection) {
-//           const elementPosition = evaluationSection.getBoundingClientRect().top;
-//           const offsetPosition = elementPosition + window.pageYOffset - offset;
-
-//           window.scrollTo({
-//             top: offsetPosition,
-//             behavior: "smooth",
-//           });
-
-//           evaluationSection.scrollTop = 0;
-//         }
-//       },
-//       { once: true }
-//     );
-//   });
-// });
 document.querySelectorAll(".nav-link").forEach((link) => {
   link.addEventListener("click", function () {
     const tabTrigger = new bootstrap.Tab(this);
     tabTrigger.show();
 
-    scrollToEvaluationSection(); // Прокрутка вниз при клике
+    scrollToEvaluationSection();
   });
 });
 
-// Делаем активную вкладку тоже кликабельной
 const activeTab = document.querySelector(".nav-link.active");
 if (activeTab) {
   activeTab.addEventListener("click", () => {
@@ -42,7 +14,6 @@ if (activeTab) {
   });
 }
 
-// Функция прокрутки
 function scrollToEvaluationSection() {
   const evaluationSection = document.querySelector("#evaluationSection");
   const offset = 20;
